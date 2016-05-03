63253d93a6bd4de8d4943bc0d031fa55af789089
Peter Marreck <lumbergh@gmail.com>
2016-05-03 13:18:56 -0400

    Added original "numeric?" function (prior to refactor), added more benchmark data

--- Words.ex
+++ Words.ex
@@ -1,23 +1,38 @@
 defmodule Words do
-  def word_freq(l) do
-    case l do
-      [] -> %{}
-      [w|ws] ->
-        counts = word_freq(ws)
-        Map.update(counts, w, 1, fn(count) -> count + 1 end)
-    end
+  # def word_freq(l) do
+  #   case l do
+  #     [] -> %{}
+  #     [w|ws] ->
+  #       counts = word_freq(ws)
+  #       Map.update(counts, w, 1, fn(count) -> count + 1 end)
+  #   end
+  # end
+
+  def word_freq([]), do: %{}
+  def word_freq([w|ws]), do: word_freq([w|ws], %{})
+  def word_freq([], counts), do: counts
+  def word_freq([w|ws], counts) do
+    word_freq(ws, Map.update(counts, w, 1, &(&1+1)))
   end
 
   # FIXME: This is very awful.
-  def alphabetic?(c) do
-    c in ?a..?z ||
-    c in ?A..?Z ||
-    c == 230
-  end
+  # def alphabetic?(c) do
+  #   c in ?a..?z ||
+  #   c in ?A..?Z ||
+  #   c == 230
+  # end
 
-  def numeric?(c) do
-    c in ?0..?9
-  end
+  def alphabetic?(c) when c in ?a..?z, do: true
+  def alphabetic?(c) when c in ?A..?Z, do: true
+  def alphabetic?(230), do: true
+  def alphabetic?(_), do: false
+
+  # def numeric?(c) do
+  #   c in ?0..?9
+  # end
+
+  def numeric?(c) when c in ?0..?9, do: true
+  def numeric?(_), do: false
 
   def alphanumeric?(c) do
     numeric?(c) || alphabetic?(c)
@@ -46,11 +61,31 @@ defmodule Words do
   end
 end
 
-defmodule Benchmark do
-  def measure(function) do
-    function
-    |> :timer.tc
-    |> elem(0)
-    |> Kernel./(1_000_000)
+# run this inline suite with "elixir #{__ENV__.file} bench"
+if System.argv |> List.first == "bench" do
+  defmodule Benchmark do
+    def measure(function) do
+      function
+      |> :timer.tc
+      |> elem(0)
+      |> Kernel./(1_000_000)
+    end
+  end
+  IO.puts "Benchmark for 'load!':"
+  IO.puts Benchmark.measure(fn -> Words.load!("data/Zen.txt") end)
+  IO.puts "Benchmark for 'load':"
+  IO.puts Benchmark.measure(fn -> Words.load("data/Zen.txt") end)
+end
+
+# run this inline suite with "elixir #{__ENV__.file} test"
+if System.argv |> List.first == "test" do
+  ExUnit.start
+
+  defmodule AlphanumericTest do
+    use ExUnit.Case, async: true
+
+    test "word_freq" do
+      assert %{"Four" => 1, "ago" => 1, "and" => 2, "four" => 1, "score" => 1, "seconds" => 1, "seven" => 1, "years" => 1} == Words.word_freq("Four score and seven years ago and four seconds" |> String.split(" "))
+    end
   end
 end
